dda873be464bb49e77d6aacb72f631344b213b3c
Vural ATICI <aticivural@gmail.com>
2019-05-06 00:34:30 +0300

    init licensing-serice and connect to config server

---
+++ src/main/java/com/vural/licensingservice/repository/LicenseRepository.java
@@ -0,0 +1,13 @@
+package com.vural.licensingservice.repository;
+
+import com.vural.licensingservice.model.License;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface LicenseRepository extends CrudRepository<License,String> {
+    List<License> findByOrganizationId(String organizationId);
+    License findByOrganizationIdAndLicenseId(String organizationId, String licenseId);
+}
---
+++ src/main/java/com/vural/licensingservice/services/OrganizationService.java
@@ -0,0 +1,7 @@
+package com.vural.licensingservice.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrganizationService {
+}
